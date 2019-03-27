8017e5367bf27d63fb64161075d712d3ce496cbe
cornel <cornel.siclovan@gmail.com>
2019-03-27 15:33:51 +0200

    second commit

---
+++ src/components/BlogPostListContainer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import BlogPostList from "./BlogPostList";
+import {blogPostList, blogPostAdd} from "../actions/actions"
+import {connect} from "react-redux"
+
+const mapStateToProps = state => ({
+    ...state.blogPostList
+});
+
+const mapDispatchToPost = {
+    blogPostList,
+    blogPostAdd
+};
+
+class BlogPostListContainer extends React.Component{
+    componentDidMount(){
+        console.log(this.props);
+        this.props.blogPostList();
+    }
+
+    render(){
+        console.log(this.props.posts)
+        return(<BlogPostList posts={this.props.posts}/>)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToPost)(BlogPostListContainer)
